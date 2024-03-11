234a2a22fac05ddd18c505a3a10b0d7ff9386c64
KnoxDefender <knoxdefenderbender@gmail.com>
2024-03-11 12:16:33 -0400

    beta
    beta

--- src/components/_help.vue
+++ src/components/_help.vue
@@ -2,7 +2,7 @@
   <div id="app" class=" rounded-2xl flex flex-col items-center justify-center h-screen bg-gradient-to-r from-orange-400 to-gray-400 text-white">
     <div class="z-10 p-12 bg-white/40 backdrop-blur-md rounded-lg border-4 border-white shadow-2xl space-y-6 text-center">
       <h1 class="text-5xl font-extrabold text-gray-800">Need <span class="text-gray-900">Help?</span></h1>
-      <a href="https://blogs.mtdv.me/knoxswebsite" class="px-8 py-3 bg-green-500 rounded-full text-white font-semibold hover:bg-blue-700 transition duration-300">
+      <a href="" class="px-8 py-3 bg-green-500 rounded-full text-white font-semibold hover:bg-blue-700 transition duration-300">
         Click Me
       </a>
     </div>
