a155bb7e4487faf321c0b51a665fe38518bddf13
KnoxDefender <knoxdefenderbender@gmail.com>
2024-03-27 16:38:49 -0400

    beta
    super dooper beta

--- src/components/_help.vue
+++ src/components/_help.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app" class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r  text-white p-8 max-w-1-0 max-w">
     <div class="z-10  backdrop-blur-md rounded-lg border-4 border-white shadow-2xl p-8 space-y-6 text-center bg-sky-100 dark:bg-gray-900">
-      <h1 class="text-5xl font-extrabold text-blue-500">Sorry, this page is curently Empty!</h1>
-      <p class="text-lg text-blue-500">Scroll through images of our Piedmontese Bulls!</p>
+<img width="94" height="94" src="https://img.icons8.com/3d-fluency/94/road-closure.png" alt="road-closure"/>      <h1 class="text-5xl font-extrabold text-blue-500">Sorry, this page is </h1>
+            <h1 class="text-5xl font-extrabold text-blue-500"> curently Empty!</h1>
+
       <div class="scroll-container">
       </div>
-      <a href="#" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-blue-800 transition duration-300">See more!</a>
+      <a href="#" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-blue-800 transition duration-300">Go Back</a>
     </div>
   </div>
 </template>
